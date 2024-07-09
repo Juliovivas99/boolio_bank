c80d97b8269bf6cf2328a78ee27afe98803944b6
Julio <julio-vivas@att.net>
2024-07-09 17:58:54 -0400

    added animations

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react"
 
 const Home = () => {
+const loggedIn = {firstName: "Julio"};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home__content">
+        <header className="home-header">
+          <HeaderBox 
+            type = "greeting"
+            title = "Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and manage your account and transactions with ease."
+          />
+
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={11234.56}
+          />
+        </header>
+      </div>
+    </section>
   );
 }
 
